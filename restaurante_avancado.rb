a9ed54d260580d820dba910cf3988d80b4b03db6
Thiago Pinto <trgpwild@gmail.com>
2013-08-28 19:46:20 -0300

    ...

--- restaurante_avancado.rb
+++ restaurante_avancado.rb
@@ -301,3 +301,18 @@ puts longest
 
 puts "-----------------------------------"
 
+caelum = [
+  {:ruby => 'rr-71', :java => 'fj-11'}, 
+  {:ruby => 'rr-75', :java => 'fj-21'} 
+]
+
+caelum.sort_by { |curso| curso[:ruby] }.each do |curso| 
+  puts "Curso de RoR na Caelum: #{ curso[:ruby] }" 
+end
+
+puts "-----------------------------------"
+
+caelum.sort_by { |curso| curso[:ruby] }.each do |curso| 
+   next if curso[:ruby] == 'rr-71'
+   puts "Curso de RoR na Caelum: #{ curso[:ruby] }" 
+end
