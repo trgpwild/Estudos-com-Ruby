a5361f86e71ee04ab6547a42b6db6754858c239f
Thiago Pinto <trgpwild@gmail.com>
2013-08-29 22:19:57 -0300

    ...

--- metaprogramacao.rb
+++ metaprogramacao.rb
@@ -1,1 +1,32 @@
-teste
+# encoding: UTF-8
+
+class Pessoa
+  # ...
+end
+
+def Pessoa.pessoas_no_mundo
+  100
+end
+
+puts Pessoa.pessoas_no_mundo
+
+class Pessoa
+
+  def self.pessoas_no_mundo
+    200
+  end
+
+end
+
+puts Pessoa.pessoas_no_mundo
+
+p = Pessoa.new
+
+def p.anda
+  puts "andando"
+end
+
+p.anda
+
+#p2 = Pessoa.new
+#p2.anda #Isso falharia porque o metodo anda somente existe na instancia 'p'
